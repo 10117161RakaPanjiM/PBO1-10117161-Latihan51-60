3686f6ed42730446c3bc9fcaa0d0a448e8cf1a0e
10117161RakaPanjiM <73197135+10117161RakaPanjiM@users.noreply.github.com>
2020-11-22 06:13:53 +0700

    Add files via upload

---
+++ PBO1-10117161-Latihan51-GajiKaryawan/src/pbo1/pkg10117161/latihan51/gajikaryawan/PBO110117161Latihan51GajiKaryawan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10117161.latihan51.gajikaryawan;
+
+/**
+ *
+ * @author GPS-2
+ */
+public class PBO110117161Latihan51GajiKaryawan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ PBO1-10117161-Latihan57-Kendaraan/src/pbo1/pkg10117161/latihan57/kendaraan/Sepeda.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10117161.latihan57.kendaraan;
+
+/**
+ *
+ * @author GPS-2
+ */
+class Sepeda {
+    
+     Sepeda umum () {
+        Sistem . keluar . println ( " Sepeda " );
+    }
+
+    public  int  getGearCount () {
+        kembalikan myGearCount;
+    }
+
+    public  void  setGearCount ( int  myGearCount ) {
+        ini . myGearCount = myGearCount;
+    }
+    
+}
